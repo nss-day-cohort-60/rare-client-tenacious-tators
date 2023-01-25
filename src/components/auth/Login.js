2c4d244439f9ee27b6c09bdb4fac4891fc03e24c
James Linkous <james.william.linkous@gmail.com>
2023-01-25 14:37:17 -0600

    commenting logic in auth directory

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -2,7 +2,9 @@ import { useRef, useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import { loginUser } from "../../managers/AuthManager"
 
+//Login function handles initializing user object, posting user to login table, and conditionally rendering JSX
 export const Login = ({ setToken }) => {
+  //useRef hook is a state variable that 1. persists between renders and 2. can change but doesn't instigate re-renders.
   const username = useRef()
   const password = useRef()
   const navigate = useNavigate()
@@ -10,18 +12,24 @@ export const Login = ({ setToken }) => {
 
   const handleLogin = (e) => {
     e.preventDefault()
-
+//initialize user object
     const user = {
       username: username.current.value,
       password: password.current.value
     }
-
+//loginUser() POSTS to login table to the API. Should this happen? Should logout delete this table?
     loginUser(user).then(res => {
+      /*
+      tests to see if the response object contains the valid property or the "valid" string
+      setToken
+      */
       if ("valid" in res && res.valid) {
+        //Sets the user into local storage and navigates to home
         setToken(res.token)
         navigate("/")
       }
       else {
+        //Used to generate a string on the form "Username or password not valid"
         setisUnsuccessful(true)
       }
     })
@@ -29,6 +37,7 @@ export const Login = ({ setToken }) => {
 
   return (
     <section className="columns is-centered">
+      {/*login form*/}
       <form className="column is-two-thirds" onSubmit={handleLogin}>
         <h1 className="title">Rare Publishing</h1>
         <p className="subtitle">Please sign in</p>
@@ -49,6 +58,7 @@ export const Login = ({ setToken }) => {
 
         <div className="field is-grouped">
           <div className="control">
+            {/*submission button*/}
             <button className="button is-link" type="submit" >Submit</button>
           </div>
           <div className="control">
