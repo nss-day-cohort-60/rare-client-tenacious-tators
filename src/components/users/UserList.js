36ec009fdf994831cff74935d3e461900633c4fa
Jessica Meeker <113938036+jesmeeker@users.noreply.github.com>
2023-01-26 15:39:06 -0600

    commented out code removed

--- src/components/users/UserList.js
+++ src/components/users/UserList.js
@@ -3,32 +3,19 @@ import { useNavigate } from 'react-router-dom'
 import { getUsers, getUsersBySearchTerm } from "../../managers/users"
 import { User } from "./User"
 import "./Users.css"
-// import { UserSearch } from "./UserSearch"
 
 export const UserList = () => {
 
   const [users, setUsers] = useState([])
-//   const [searchTerm, setSearchTerm] = useState('')
   const navigate = useNavigate()
 
   useEffect(() => {
     getUsers().then(usersData => setUsers(usersData))
   }, [])
-//   useEffect(() => {
-//     if (searchTerm.length > 1) {
-//       getUsersBySearchTerm(searchTerm).then((usersData) => setUsers(usersData))
-//     } else {
-//       getUsers().then((usersData) => setUsers(usersData))
-//     }
-//   }, [searchTerm])
 
-//   const onSearchTermChange = (value) => {
-//     setSearchTerm(value)
-//   }
 
   return (
     <>
-      {/* <UserSearch onSearchTermChange={onSearchTermChange} searchTerm={searchTerm} /> */}
       <div style={{ marginTop: "2rem" }}>
         <div className="users">
           {
