f2d0fe2ee0cd686a9e798a03c7ecb2673cf8b941
Lauren Hanson <lohanson33@gmail.com>
2023-01-27 13:51:50 -0600

    update

--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -1,3 +1,13 @@
+<<<<<<< HEAD
+import { Route, Routes } from "react-router-dom";
+import { Login } from "../components/auth/Login";
+import { Register } from "../components/auth/Register";
+import { Authorized } from "./Authorized";
+import { MyPosts } from "../components/posts/MyPosts";
+import { Categories } from "../components/categories/Categories";
+import { Tags } from "../components/tags/Tag";
+import { PostList } from "../components/posts/PostsList";
+=======
 import { Route, Routes } from "react-router-dom"
 import { Login } from "../components/auth/Login"
 import { Register } from "../components/auth/Register"
@@ -8,10 +18,31 @@ import { Categories } from "../components/categories/Categories"
 import { Tags } from "../components/tags/Tag"
 import { TagList } from "../components/tags/TagList"
 import { UserList } from "../components/users/UserList"
+>>>>>>> main
 
 // receiving 2 props from Rare.js
 // responsible for routing users to specific views depending on URL paths
 export const ApplicationViews = ({ token, setToken }) => {
+<<<<<<< HEAD
+  return (
+    <>
+      <Routes>
+        {/* creating path for login & register */}
+        {/* passing setToken prop from Rare.js to Login & Register  */}
+        <Route path="/login" element={<Login setToken={setToken} />} />
+        <Route path="/register" element={<Register setToken={setToken} />} />
+        <Route element={<Authorized token={token} />}>
+          {/* Add Routes here */}
+          <Route path="/posts" element={<PostList />} />
+          <Route path="/myposts" element={<MyPosts />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/tags" element={<Tags />} />
+        </Route>
+      </Routes>
+    </>
+  );
+};
+=======
   return <>
     <Routes>
       {/* creating path for login & register */}
@@ -35,3 +66,4 @@ export const ApplicationViews = ({ token, setToken }) => {
     </Routes>
   </>
 }
+>>>>>>> main
