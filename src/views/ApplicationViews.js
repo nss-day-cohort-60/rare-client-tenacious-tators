adb8e321340f96ef181006e8f30b7248ab3a0188
Jessica Meeker <113938036+jesmeeker@users.noreply.github.com>
2023-01-27 16:10:46 -0600

    main fixes

--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -21,13 +21,13 @@ export const ApplicationViews = ({ token, setToken }) => {
       <Route path="/register" element={<Register setToken={setToken} />}  />
       <Route element={<Authorized token={token} />}>
         {/* Add Routes here */}
-        <Route path="/posts/" >
-          <Route element={<Posts />} />
+        <Route path="/posts" >
+          <Route index element={<PostList />} />
           <Route path="myposts" element={<MyPosts />} />
-          <Route path="details/:postId" element={<PostDetail token={token} />} />
+          <Route path=":postId" element={<PostDetail token={token} />} />
         </Route>
         <Route path="/categories" element={<Categories />} />
-        <Route path="/tags" element={<Tags />} />
+        <Route path="/tags" element={<TagList />} />
         <Route path="/users">
           <Route index element={<UserList />} />
           <Route path=":userId" element={<UserDetails />} />
