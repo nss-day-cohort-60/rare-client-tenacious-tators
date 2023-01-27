0b9af01cef9e72c0314c32525c23c604f9ebaa0e
Nick Valdez <nicholastvaldez@gmail.com>
2023-01-27 11:33:10 -0600

    add all posts

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -1,9 +1,13 @@
-export const Posts = () => {
+import { Link } from "react-router-dom";
 
-  
-  return (
-    <>
-      <h1>Post</h1>
-    </>
-  );
-};
+export const Posts = ({ post }) => (
+  <tr>
+    <td>{post.title}</td>
+    <td>
+      {post?.author?.first_name}
+      {post?.author?.last_name}
+    </td>
+    <td>{post.publication_date}</td>
+    <td>{post?.category?.label}</td>
+  </tr>
+);
--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -1,35 +1,30 @@
-import { Route, Routes } from "react-router-dom"
-import { Login } from "../components/auth/Login"
-import { Register } from "../components/auth/Register"
-import { Authorized } from "./Authorized"
-import { Posts } from "../components/posts/Posts"
-import { MyPosts } from "../components/posts/MyPosts"
-import { Categories } from "../components/categories/Categories"
-import { Tags } from "../components/tags/Tag"
-import { UserList } from "../components/users/UserList"
+import { Route, Routes } from "react-router-dom";
+import { Login } from "../components/auth/Login";
+import { Register } from "../components/auth/Register";
+import { Authorized } from "./Authorized";
+import { MyPosts } from "../components/posts/MyPosts";
+import { Categories } from "../components/categories/Categories";
+import { Tags } from "../components/tags/Tag";
+import { PostList } from "../components/posts/PostsList";
 
 // receiving 2 props from Rare.js
 // responsible for routing users to specific views depending on URL paths
 export const ApplicationViews = ({ token, setToken }) => {
-  return <>
-    <Routes>
-      {/* creating path for login & register */}
-      {/* passing setToken prop from Rare.js to Login & Register  */}
-      <Route path="/login" element={<Login setToken={setToken} />}  />
-      <Route path="/register" element={<Register setToken={setToken} />}  />
-      <Route element={<Authorized token={token} />}>
-        {/* Add Routes here */}
-      <Route path="/posts" element={<Posts />} />
-      <Route path="/myposts" element={<MyPosts />} />
-      <Route path="/categories" element={<Categories />} />
-      <Route path="/tags" element={<Tags />} />
-      
-      <Route path="/users">
-        <Route index element={<UserList />} />
-      </Route>
-
-        
-      </Route>
-    </Routes>
-  </>
-}
+  return (
+    <>
+      <Routes>
+        {/* creating path for login & register */}
+        {/* passing setToken prop from Rare.js to Login & Register  */}
+        <Route path="/login" element={<Login setToken={setToken} />} />
+        <Route path="/register" element={<Register setToken={setToken} />} />
+        <Route element={<Authorized token={token} />}>
+          {/* Add Routes here */}
+          <Route path="/posts" element={<PostList />} />
+          <Route path="/myposts" element={<MyPosts />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/tags" element={<Tags />} />
+        </Route>
+      </Routes>
+    </>
+  );
+};
