d10f3fb7668a63a29d05ace144383b609c0a4cb9
Nick Valdez <nicholastvaldez@gmail.com>
2023-01-30 13:26:47 -0600

    add users posts in myposts

--- src/components/posts/MyPosts.js
+++ src/components/posts/MyPosts.js
@@ -1,24 +1,33 @@
-import { useEffect, useState } from "react";
-import "./MyPosts.css";
+import React, { useEffect, useState } from "react";
+import { getCurrentUserPosts } from "../../managers/Posts";
+import "./Posts.css";
 
 export const MyPosts = ({ token }) => {
   const [posts, setPosts] = useState([]);
-
   const tokenInt = parseInt(token);
+
   useEffect(() => {
-    fetch(`http://localhost:8088/posts?userId=${tokenInt}`)
-      .then((res) => res.json())
-      .then((postData) => {
-        setPosts(postData);
-      });
+    getCurrentUserPosts(tokenInt).then((postData) => {
+      setPosts(postData);
+    });
   }, []);
+
   return (
-    <>
-      <div className="post-card">
-        <h1>{posts.title}</h1>
-        <h3>{posts.publication_date}</h3>
-        <img src={posts.image_url} alt="Post Description"></img>
-      </div>
-    </>
+    <div className="user-posts">
+      {posts.map((post) => (
+        <>
+          <h1>{post.title}</h1>
+          <h2>{post?.user?.username}</h2>
+          <h3>{post?.category?.label}</h3>
+          <h3>{post.publication_date}</h3>
+          <p>{post.content}</p>
+
+          <div className="buttons">
+            <button onClick={() => {}}>DELETE</button>
+            <button onClick={() => {}}>EDIT</button>
+          </div>
+        </>
+      ))}
+    </div>
   );
 };
--- src/managers/Posts.js
+++ src/managers/Posts.js
@@ -1,3 +1,9 @@
 export const getPosts = () => {
   return fetch("http://localhost:8088/posts").then((res) => res.json());
 };
+
+export const getCurrentUserPosts = (id) => {
+  return fetch(`http://localhost:8088/posts?user_id=${id}`).then((res) =>
+    res.json()
+  );
+};
