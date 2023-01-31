fe237ee4403d49252fd187ff691dba9f603e912b
James Linkous <james.william.linkous@gmail.com>
2023-01-31 16:09:01 -0600

    unsubscribe button renders conditionally and deletes from subscriptions table

--- src/components/users/subscribe.js
+++ src/components/users/subscribe.js
@@ -1,26 +1,59 @@
-import { addSubscription } from "../../managers/subscriptions"
-import { useNavigate } from 'react-router-dom'
+import { addSubscription, deleteSubscription } from "../../managers/subscriptions"
+import { useNavigate, useParams } from 'react-router-dom'
+import { useEffect, useState } from "react"
 
-export const SubscriptionForm = ({ token, authorObject}) => {
-
+export const SubscriptionForm = ({ token, authorId }) => {
+  const [userSubscriptions, setUserSubscriptions] = useState({})
   const tokenInt = parseInt(token)
   const navigate = useNavigate()
   const subscription = {
     follower_id: tokenInt,
-    author_id: authorObject.id
+    author_id: authorId
   }
 
-  const createNewSubscription = () => {
-        const copy = {...subscription}
-        copy.created_on = Date.now()
+  useEffect(()=>{
+    fetch(`http://localhost:8088/subscriptions?follower_id=${token}`)
+    .then(res=>res.json())
+    .then(res=>{
+      const filteredCopy = res.filter(obj=>obj.author_id === parseInt(authorId))
+      console.log(filteredCopy)
+      
+      setUserSubscriptions(filteredCopy[0])
+    })}
+  , [token])
 
-          addSubscription(copy)
-          .then(() => navigate(`/`))
+  const createNewSubscription = () => {
+    const copy = {...subscription}
+    copy.created_on = Date.now()
+      addSubscription(copy)
+      .then(() => navigate(`/`))
     }
-    return <button type="submit"
-    onClick={evt => {
-        evt.preventDefault()
-        createNewSubscription()
-    }}
-    className="subscribeBtn">Subscribe</button>
-}
+  const unsubscribe = (id) => {
+    deleteSubscription(id)
+    .then(()=>{navigate(`/`)})
+  }
+console.log(userSubscriptions)
+    return <>
+      {userSubscriptions
+      ?<button 
+      type="submit"
+      className="subscribeBtn"
+      onClick={evt => {
+          evt.preventDefault()
+          unsubscribe(userSubscriptions.id)
+      }}>
+        Unsubscribe
+      </button>
+      :
+      <button 
+        type="submit"
+        className="subscribeBtn"
+        onClick={evt => {
+            evt.preventDefault()
+            createNewSubscription()
+        }}>
+          Subscribe
+        </button>
+        }
+      </>
+  }
