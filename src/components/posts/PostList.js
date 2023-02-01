8513ebabdeec1941fc758d85bfe3fc5e68763560
Jessica Meeker <113938036+jesmeeker@users.noreply.github.com>
2023-02-01 10:58:55 -0600

    Filter by category drop down added to PostList.js

--- src/components/posts/PostList.js
+++ src/components/posts/PostList.js
@@ -3,17 +3,44 @@ import { useNavigate } from "react-router-dom";
 import { Post, Posts, TableRow } from "./Posts";
 import { getPosts } from "../../managers/Posts";
 import "./Posts.css";
+import { getCategories } from "../../managers/categories";
 
 export const PostList = ({ token }) => {
   const [posts, setPosts] = useState([]);
+  const [filteredPosts, setFilteredPosts] = useState([]);
+  const [categories, setCategories] = useState([])
+  const [selectedCategory, setSelectedCategory] = useState(0)
   const navigate = useNavigate();
+
+  useEffect(
+    () => {
+        getCategories().then((categoryData) => setCategories(categoryData))
+    }, [])
 
   useEffect(() => {
     getPosts().then((postData) => setPosts(postData));
   }, []);
 
+  useEffect(
+    () => {
+      if (selectedCategory === 0) {setFilteredPosts(posts)}
+      else {
+        const filteredCopy = posts.filter(post => post.category_id === parseInt(selectedCategory))
+        setFilteredPosts(filteredCopy)
+        }
+    },
+    [posts, selectedCategory]
+)
   return (
     <>
+        <select name="categoryId" className="form-control" value="category_id" onChange={(event) => {setSelectedCategory(event.target.value)}}>
+          <option value="0">Search by Category</option>
+          {categories.map(category => (
+              <option key={`category--${category.id}`} value={category.id}>
+                  {category.label}
+              </option>
+          ))}
+      </select>
       <div className="addPostButton">
         Add Post <button onClick={() => navigate("newpost")}>+</button>
       </div>
@@ -30,7 +57,7 @@ export const PostList = ({ token }) => {
             </tr>
           </thead>
           <tbody>
-            {posts.map((post) => (
+            {filteredPosts.map((post) => (
               <Posts key={post.id} post={post} token={token} />
             ))}
           </tbody>
