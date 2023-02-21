8628e5b77dc432bb27f216f259f3411cb51b1a65
Maegan Phan <maegyoung@gmail.com>
2023-02-21 10:54:13 -0600

    added auth to tags manager

--- src/managers/tags.js
+++ src/managers/tags.js
@@ -1,6 +1,10 @@
 
 export const getTags = () => {
-    return fetch("http://localhost:8000/tags")
+    return fetch("http://localhost:8000/tags", {
+      headers: {
+        "Authorization": `Token ${localStorage.getItem("rare_token")}`
+      },
+    })
         .then(res => res.json())
 }
 
