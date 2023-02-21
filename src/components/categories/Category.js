ed13855ed1ec35c06ba5ae15f07751a67ab06971
Emily Nelson <evnelson2021@gmail.com>
2023-02-21 15:11:13 -0600

    working on it

--- src/components/categories/Category.js
+++ src/components/categories/Category.js
@@ -1,16 +1,44 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import "./Categories.css"
+import { getCategories, updateCategory } from "../../managers/categories.js"
+import { deleteCategory } from "../../managers/categories"
+import { useEffect, useState } from "react"
 
-export const Category = ({ category }) => (
-    <article className="categories">
-        <div>
-            <Link to={`/categories/${category.id}`}>
-                <h3>{category.label}</h3>
-            </Link>
-                <button className="editButton">Edit</button>
-                <button className="deleteButton">Delete</button>
-        </div>
-    </article>
+export const Category = ({ category }) => {
+    const [ categories, setCategories ] = useState([
+        {id: 0,
+        label: ""
+        }
+    ])
+    const navigate = useNavigate()
+    
+    useEffect(() => {
+        getCategories().then(data => setCategories(data))
+    }, [])
+
+    const deleteButton = (id) => {
+        return <button onClick={() => {
+            deleteCategory(id)
+            .then(() => {
+                    getCategories().then(data => setCategories(data))
+                })
+        }} className="deleteButton">Delete</button>
+    }
+
+    return(
+        <article className="categories">
+            <div>
+                <Link to={`/categories/${category.id}`}>
+                    <h3>{category.label}</h3>
+                </Link>
+                <button className="editButton"
+                    onClick={() => {
+                        navigate({ pathname: `edit/${category.id}` })
+                        }}>Edit</button>
+                {deleteButton(category.id)}
+            </div>
+        </article>
     )
+}
 
 
