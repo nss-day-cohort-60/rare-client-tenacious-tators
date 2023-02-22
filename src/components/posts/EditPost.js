d97f7cf8b9121363d1a51dc5d7e697de9454fe35
Lauren Hanson <lohanson33@gmail.com>
2023-02-22 11:20:31 -0600

    new main

--- src/components/posts/EditPost.js
+++ src/components/posts/EditPost.js
@@ -6,6 +6,7 @@ import { editPost, getSinglePost } from "../../managers/Posts";
 export const EditPost = ({ token }) => {
   const [post, setPost] = useState({});
   const [categories, setCategories] = useState([]);
+  const [newTags, setNewTags] = useState([])
   const { postId } = useParams();
   const navigate = useNavigate();
 
@@ -27,86 +28,96 @@ export const EditPost = ({ token }) => {
     editPost(postId, post).then(() => navigate("/posts"));
   };
 
-  return (
-    <form className="addNewPostForm">
-      <h2>Edit Post</h2>
-      <fieldset>
-        <div className="form-group">
-          <input
-            type="text"
-            name="title"
-            required
-            autoFocus
-            defaultValue={post.title}
-            className="form-control"
-            placeholder="Title"
-            onChange={handleNewPostInfo}
-          />
-        </div>
-      </fieldset>
-      <fieldset>
-        <div className="form-group">
-          <input
-            type="text"
-            name="imageUrl"
-            required
-            defaultValue={post.image_url}
-            autoFocus
-            className="form-control"
-            placeholder="ImageURL"
-            onChange={handleNewPostInfo}
-          />
-        </div>
-      </fieldset>
-      <fieldset>
-        <div className="form-group">
-          <textarea
-            type="textbox"
-            rows="5"
-            cols="30"
-            name="content"
-            required
-            defaultValue={post.content}
-            autoFocus
-            className="form-control"
-            placeholder="Article Content"
-            onChange={handleNewPostInfo}
-          />
-        </div>
-      </fieldset>
-      <fieldset>
-        <div className="form-group">
-          <select
-            name="categoryId"
-            className="form-control"
-            value={post.categoryId}
-            onChange={(event) => {
-              const copy = { ...post };
-              copy.categoryId = parseInt(event.target.value);
-              setPost(copy);
-            }}
-          >
-            <option defaultValue={post.category_id}>
-              {post?.category?.label}
+
+  const tagArray = (tagId) => {
+    let copy = new Set(newTags)
+    copy.has(tagId)
+    copy.delete(tagId) : copy.add(tagId)
+
+setNewTags(copy)
+    }
+
+
+return (
+  <form className="addNewPostForm">
+    <h2>Edit Post</h2>
+    <fieldset>
+      <div className="form-group">
+        <input
+          type="text"
+          name="title"
+          required
+          autoFocus
+          defaultValue={post.title}
+          className="form-control"
+          placeholder="Title"
+          onChange={handleNewPostInfo}
+        />
+      </div>
+    </fieldset>
+    <fieldset>
+      <div className="form-group">
+        <input
+          type="text"
+          name="imageUrl"
+          required
+          defaultValue={post.image_url}
+          autoFocus
+          className="form-control"
+          placeholder="ImageURL"
+          onChange={handleNewPostInfo}
+        />
+      </div>
+    </fieldset>
+    <fieldset>
+      <div className="form-group">
+        <textarea
+          type="textbox"
+          rows="5"
+          cols="30"
+          name="content"
+          required
+          defaultValue={post.content}
+          autoFocus
+          className="form-control"
+          placeholder="Article Content"
+          onChange={handleNewPostInfo}
+        />
+      </div>
+    </fieldset>
+    <fieldset>
+      <div className="form-group">
+        <select
+          name="categoryId"
+          className="form-control"
+          value={post.categoryId}
+          onChange={(event) => {
+            const copy = { ...post };
+            copy.categoryId = parseInt(event.target.value);
+            setPost(copy);
+          }}
+        >
+          <option defaultValue={post.category_id}>
+            {post?.category?.label}
+          </option>
+          {categories.map((category) => (
+            <option key={`category--${category.id}`} value={category.id}>
+              {category.label}
             </option>
-            {categories.map((category) => (
-              <option key={`category--${category.id}`} value={category.id}>
-                {category.label}
-              </option>
-            ))}
-          </select>
-        </div>
-      </fieldset>
-      <button
-        type="publish"
-        onClick={(evt) => {
-          evt.preventDefault();
-          publishNewArticle();
-        }}
-        className="publishButton"
-      >
-        Publish
-      </button>
-    </form>
-  );
+          ))}
+        </select>
+      </div>
+    </fieldset>
+    <button
+      type="publish"
+      onClick={(evt) => {
+        evt.preventDefault();
+        publishNewArticle();
+      }}
+      className="publishButton"
+    >
+      Publish
+    </button>
+  </form>
+);
 };
