588d3aafdc4d9128a7a006c681b62f9bdec611c7
Maegan Phan <maegyoung@gmail.com>
2023-02-27 15:29:10 -0600

    finishing touches

--- src/components/comments/CommentList.js
+++ src/components/comments/CommentList.js
@@ -1,17 +1,23 @@
 import { useEffect, useState } from "react"
 import { useNavigate, useParams } from "react-router-dom"
 import { getCommentsByPostId } from "../../managers/comments"
+import { getSinglePost } from "../../managers/Posts"
 import { Comment } from "./Comment"
 import "./comments.css"
 
 export const CommentList = ({ token }) => {
   const [comments, setComments] = useState([])
+  const [post, setPost] = useState([])
   const navigate = useNavigate()
 
   const { postId } = useParams()
 
   useEffect(() => {
     getCommentsByPostId(postId).then((postData) => setComments(postData))
+  }, [])
+
+  useEffect(() => {
+    getSinglePost(postId).then((postData) => setPost(postData))
   }, [])
 
   return (
@@ -22,7 +28,7 @@ export const CommentList = ({ token }) => {
         <button className="button is-rounded is-small" onClick={() => navigate(`/posts/${postId}/comment`)}>
                     ADD COMMENT
                   </button></div>
-          <div className="comments_title">Comments</div>
+          <div className="comments_title">Comments for <i>{post.title}</i></div>
           {comments.map((comment) => (
             <Comment key={comment.id} comment={comment} />
           ))}
