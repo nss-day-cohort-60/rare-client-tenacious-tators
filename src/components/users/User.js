e9f5a933585d3b7d328a54e6171e10e229432c7a
Jessica Meeker <113938036+jesmeeker@users.noreply.github.com>
2023-02-27 11:49:04 -0600

    stash

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom"
+import { SubscriptionForm } from "./subscribe"
 
-export const User = ({ user }) => (
-    <section className="user">
+export const User = ({ user , setUsers }) => (
+    <section className="user__card" key={user.id}>
+        <img src={user.profile_image_url} className="user card__image"/>
         <h3 className="user__name">
                 <Link to={`/users/${user.user.id}`}>
-                    <h3>{ user.user.username }</h3>
+                    <h3>{ user.full_name }</h3>
                 </Link>
-                <div>{ user.full_name }</div>
+                <div>{ user.user.username } # of followers</div>
                 <div>{ user.user.email }</div>
+                <SubscriptionForm user={user} setUsers={setUsers}/>
+
         </h3>
     </section>
 )
