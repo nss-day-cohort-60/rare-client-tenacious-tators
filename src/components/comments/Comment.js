26cf13f31dd549fc4b260be866405cd21b308dd4
Maegan Phan <maegyoung@gmail.com>
2023-02-27 11:53:12 -0600

    new main

--- src/components/comments/Comment.js
+++ src/components/comments/Comment.js
@@ -8,7 +8,7 @@ export const Comment = ({ comment }) => {
   return (
 
     <section className="comments__card">
-      <div className="comment__author"><img className="comment__image" src={comment?.author?.profile_image_url}/>{comment?.author?.full_name} </div>
+      <div className="comment__author"><img className="comment__image" src={comment?.author?.profile_image_url}/>{comment?.author?.full_name}</div>
       <div className="comment__content">"{comment?.content}"</div>
       {comment.writer ?
         <div>
